439dc78a81307ac5c1b6f44dd77b695bd6d7f2c2
Quinn Cassella <sqcassella@gmail.com>
2024-03-04 20:39:52 -0800

    Did some noodling around. It now runs propperly and spawns a rect covering all of the LoL window

--- Main.py
+++ Main.py
@@ -9,7 +9,29 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 
 league_window_name = "League of Legends"
 
+def get_window_rect(window_title):
+        hwnd = win32gui.FindWindow(None, window_title)
+        if hwnd:
+            rect = win32gui.GetWindowRect(hwnd)
+            return rect
+        else:
+            return None   
+def get_open_windows():
+    windows = []
+def enum_windows_callback(hwnd, window_list):
+    if win32gui.IsWindowVisible(hwnd):
+        window_text = win32gui.GetWindowText(hwnd)
+        if window_text:
+            window_list.append((hwnd, window_text))
+    win32gui.EnumWindows(enum_windows_callback, windows)
+    return windows
 
+def alignWindowToLoL(self):
+    x,y,z,zz = get_window_rect(league_window_name)
+    self.setGeometry(x,y,z,zz)
+def mousePressEvent(self, event):
+    print('click!')
+    #QtWidgets.qApp.quit()
 
 class MainWindow(QMainWindow):
     
@@ -21,39 +43,13 @@ class MainWindow(QMainWindow):
             QtCore.Qt.FramelessWindowHint |
             QtCore.Qt.X11BypassWindowManagerHint
         )
-        self.setGeometry(
-            QtWidgets.QStyle.alignedRect(
-            QtCore.Qt.LeftToRight, QtCore.Qt.AlignCenter,
-            QtCore.QSize(220, 32),
-            QtWidgets.qApp.desktop().availableGeometry()
-        ))
-    def get_window_rect(window_title):
-        hwnd = win32gui.FindWindow(None, window_title)
-        if hwnd:
-            rect = win32gui.GetWindowRect(hwnd)
-            return rect
-        else:
-            return None   
+        alignWindowToLoL(self)
+        
 
-    def get_open_windows():
-        windows = []
-
-        def enum_windows_callback(hwnd, window_list):
-            if win32gui.IsWindowVisible(hwnd):
-                window_text = win32gui.GetWindowText(hwnd)
-                if window_text:
-                    window_list.append((hwnd, window_text))
-
-        win32gui.EnumWindows(enum_windows_callback, windows)
-        return windows
-    
-    def alignWindowToLoL(self):
-        x,y,z,zz = get_window_rect(league_window_name)
-        self.setGeometry(x,y,z,zz)
-
-    def mousePressEvent(self, event):
-        print('click!')
-        #QtWidgets.qApp.quit()
-
-   
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mywindow = MainWindow()
+    #mywindow.showMaximized()
+    mywindow.show()
+    app.exec_()
 
