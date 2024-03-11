01e5897aa2adcd1219ee0a31532ab1cb597a16dc
Quinn Cassella <sqcassella@gmail.com>
2024-03-10 19:00:08 -0700

    Added some error checking

--- Prototype.py
+++ Prototype.py
@@ -14,6 +14,11 @@ league_window_name = "League of Legends"
 
 poss = [[722, 316, 722, 316],[922, 316, 922, 316],[722, 516, 722, 516],[922, 516, 922, 516]]
 
+# This list was written with the assumption that no single item will cost more than 4500 RP.    
+# First RP then price in USD with no tax.
+usd_RP = [[575, 4.99], [1380, 10.99], [2800, 21.99], [4500, 34.99]]
+
+
 def get_RP():
     return read_number(league_window_offset(932, 18, 995, 42))
     # read_number(get_window_rect(league_window_name))
@@ -28,6 +33,12 @@ def get_default_shop_prices():
     holder.append(read_number(league_window_offset(922, 316, 922 + w, 316 + l)))
     holder.append(read_number(league_window_offset(722, 516, 722 + w, 516 + l)))
     holder.append(read_number(league_window_offset(922, 516, 922 + w, 516 + l)))
+    for i in holder:
+        try:
+            int(i)
+        except text_or_error:
+            i = 0
+
     return holder
 
 def league_window_offset(x1, y1, x2, y2) :
@@ -83,6 +94,7 @@ def covert_RP_from(inp, cur):
     inp = round(inp*cur*100, 2)
     return str(inp)
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
@@ -102,16 +114,7 @@ class MainWindow(QMainWindow):
         print('click!')
         QtWidgets.qApp.quit()
 
-
-if __name__ == '__main__':
-    get_default_shop_prices()
-    usd = 1.15
-    app = QApplication(sys.argv)
-    layout = QtWidgets.QVBoxLayout()
-    mywindow = MainWindow()
-    rp = get_RP()
-    shop_prices = get_default_shop_prices()
-    
+def load_window(mywindow, layout, shop_prices):
     label = QtWidgets.QLabel(convert_RP_to(int(rp),usd), mywindow)
     label.setStyleSheet(
         "color: #F0E6D2; background-color: #010710; padding-left: 2px; padding-bottom: 5px; font-size: 14px; font-weight: bold;")
@@ -120,14 +123,22 @@ if __name__ == '__main__':
     #label1 = QtWidgets.QLabel("0", mywindow)
     y = 0
     for i in shop_prices:
-    
         holder_widget = QtWidgets.QLabel(convert_RP_to(int(i),usd), mywindow)
         layout.addWidget(holder_widget)
-        holder_widget.move(poss[y][0] - 30, poss[y][1] - 5)
+        holder_widget.move(poss[y][0], poss[y][1])
         holder_widget.setStyleSheet(
-        "color: red; background-color: #010710; padding-left: 2px; padding-bottom: 0px; font-size: 12px; font-weight: bold;")
+            "color: #F0E6D2; background-color: #010710; padding-left: 2px; padding-bottom: 0px; font-size: 14px; font-weight: bold;")
         y += 1
 
+if __name__ == '__main__':
+    usd = 1.15
+    app = QApplication(sys.argv)
+    layout = QtWidgets.QVBoxLayout()
+    mywindow = MainWindow()
+    rp = get_RP()
+    shop_prices = get_default_shop_prices()
+
+    load_window(mywindow, layout, shop_prices)
     
     #layout.addWidget()
     mywindow.show()
